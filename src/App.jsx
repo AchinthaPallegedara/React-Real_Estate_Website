05345efa202e67af391627ca776c891d82c53b1b
Achintha Hirudika <achinthahirudika@gmail.com>
2023-05-10 21:38:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+      <Header />
+      <Hero />
+      </div>
+      <Companies />
+  
+    </div>
   );
 }
 
